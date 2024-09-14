ea38d8a56b4f74cb6e8d6fba721ec51af25b100a
tyang1567 <yangtiffany78@gmail.com>
2024-09-13 21:59:35 -0400

    Initial commit

---
+++ app/src/main/java/yang/tiffany/prework/MainActivity.kt
@@ -0,0 +1,29 @@
+package yang.tiffany.prework
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.helloButton)
+        val button = findViewById<Button>(R.id.helloButton)
+        button.setOnClickListener{
+            Log.v("Helloworld", "Button clicked!")
+            Toast.makeText(this, "Hi hi!", Toast.LENGTH_SHORT).show()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
